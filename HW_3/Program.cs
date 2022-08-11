0b48e7fac3d65ca204b40b06497078e620716367
Alexander_Napreenko <sasha-napreenko@yandex.ru>
2022-08-11 19:49:56 +0300

    Add HW_3

---
+++ HW_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+            int number = 10;
+
+            Console.WriteLine("Введите число:");
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 1)
+            {
+                Console.WriteLine("Число " + number + " нечётное");
+            }
+            else
+            {
+                Console.WriteLine("Число " + number + " чётное");
+            }
